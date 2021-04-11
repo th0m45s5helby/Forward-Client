e1b0422aae2456aa1914c83187f845e60c8e1907
thomas shelby <70005133+th0m45s5helby@users.noreply.github.com>
2021-04-11 15:49:18 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,8 +10,8 @@ class Config(object):
     # Get This From @StringSessionGen_Bot
     STRING_SESSION = os.environ.get("STRING_SESSION")
     # Forward From Chat ID
-    FORWARD_FROM_CHAT_ID = os.environ.get("FORWARD_FROM_CHAT_ID", None)
+    FORWARD_FROM_CHAT_ID = "-1001384761806"
     # Forward To Chat ID
-    FORWARD_TO_CHAT_ID = os.environ.get("FORWARD_TO_CHAT_ID", None)
+    FORWARD_TO_CHAT_ID = "-1001405478529"
     # Your User ID
-    USER_ID = os.environ.get("USER_ID", None)
+    USER_ID = "1722532247"
