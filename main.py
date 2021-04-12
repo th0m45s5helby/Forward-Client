8a1948119ddf0a2b3edd2fc73f4bbb47d9a70e5e
thomas shelby <70005133+th0m45s5helby@users.noreply.github.com>
2021-04-12 18:10:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from configs import Config
 
 User = Client(session_name=Config.STRING_SESSION, api_hash=Config.API_HASH, api_id=Config.API_ID)
 async def forward():
-    async for message in User.iter_history(chat_id=-1001177624846,reverse=True):
+    async for message in User.iter_history(chat_id=-1001487976173,reverse=True):
         try:
             await message.copy(int(Config.FORWARD_TO_CHAT_ID))
         except FloodWait as e:
-            await client.send_message(chat_id="me", text=f"#FloodWait: Stopping Forwarder for `{e.x}s`!")
+            await User.send_message(chat_id="me", text=f"#FloodWait: Stopping Forwarder for `{e.x}s`!")
             await asyncio.sleep(e.x)
         except Exception as err:
-            await client.send_message(chat_id="me", text=f"#ERROR: `{err}`")
+            await User.send_message(chat_id="me", text=f"#ERROR: `{err}`")
 
 
 
