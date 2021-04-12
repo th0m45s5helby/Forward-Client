bbfacdb49c8521cec125d934b9dacd108620cc76
thomas shelby <70005133+th0m45s5helby@users.noreply.github.com>
2021-04-12 17:21:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from pyrogram.errors import FloodWait
 from configs import Config
 
 User = Client(session_name=Config.STRING_SESSION, api_hash=Config.API_HASH, api_id=Config.API_ID)
-for message in User.iter_history(-1001177624846):
-    try:
-        message.copy(int(Config.FORWARD_TO_CHAT_ID))
-    except FloodWait as e:
-        await client.send_message(chat_id="me", text=f"#FloodWait: Stopping Forwarder for `{e.x}s`!")
-        await asyncio.sleep(e.x)
-    except Exception as err:
-        await client.send_message(chat_id="me", text=f"#ERROR: `{err}`")
+async def forward():
+    for message in User.iter_history(-1001177624846):
+        try:
+            message.copy(int(Config.FORWARD_TO_CHAT_ID))
+        except FloodWait as e:
+            await client.send_message(chat_id="me", text=f"#FloodWait: Stopping Forwarder for `{e.x}s`!")
+            await asyncio.sleep(e.x)
+        except Exception as err:
+            await client.send_message(chat_id="me", text=f"#ERROR: `{err}`")
 
 
 
@@ -28,7 +29,8 @@ async def main(client, message):
 #         return
 
     if message.text == "!start" and message.from_user.id == int(Config.USER_ID):
-        await message.edit(text="Hi, Myself!\nI am Alive XD", parse_mode="Markdown", disable_web_page_preview=True)
+        forward()
+        #await message.edit(text="Hi, Myself!\nI am Alive XD", parse_mode="Markdown", disable_web_page_preview=True)
     elif message.text == "!help" and message.from_user.id == int(Config.USER_ID):
         await message.edit(
             text="This UserBot can forward messages from any Chat to any other Chat XD\n\nDeveloper: @AbirHasan2005",
