ffc7379257934b0663f30f958bd19b5ab46ed13a
thomas shelby <70005133+th0m45s5helby@users.noreply.github.com>
2021-04-12 17:58:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from configs import Config
 
 User = Client(session_name=Config.STRING_SESSION, api_hash=Config.API_HASH, api_id=Config.API_ID)
 async def forward():
-    async for message in User.iter_history(-1001177624846):
+    async for message in User.iter_history(chat_id=-1001177624846,offset=2191,reverse=True):
         try:
             await message.copy(int(Config.FORWARD_TO_CHAT_ID))
         except FloodWait as e:
