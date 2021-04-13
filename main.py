b9343085cbd85567f5b7abc3e82b61e95f2a8272
thomas shelby <70005133+th0m45s5helby@users.noreply.github.com>
2021-04-13 11:38:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 
 User = Client(session_name=Config.STRING_SESSION, api_hash=Config.API_HASH, api_id=Config.API_ID)
-async def forward():
+'''async def forward():
     async for message in User.iter_history(chat_id=-1001441836066):
         try:
             await message.copy(int(Config.FORWARD_TO_CHAT_ID))
@@ -18,7 +18,7 @@ async def forward():
             await asyncio.sleep(e.x)
         except Exception as err:
             await User.send_message(chat_id="me", text=f"#ERROR: `{err}`")
-    await User.send_message(chat_id="me",text="Channel files successfully kanged")
+    await User.send_message(chat_id="me",text="Channel files successfully kanged")'''
 
 
 
@@ -31,13 +31,13 @@ async def main(client, message):
 #         return
 
     if message.text == "!start" and message.from_user.id == int(Config.USER_ID):
-        await forward()
+        await message.edit(text="Don't worry I am alive",parse_mode="Markdown",disable_web_page_preview=True)
         #await message.edit(text="Hi, Myself!\nI am Alive XD", parse_mode="Markdown", disable_web_page_preview=True)
     elif message.text == "!help" and message.from_user.id == int(Config.USER_ID):
         await message.edit(
             text="This UserBot can forward messages from any Chat to any other Chat XD\n\nDeveloper: @AbirHasan2005",
             parse_mode="Markdown", disable_web_page_preview=True)
-    '''elif message.chat.id in [-1001489565747,-1001469623910,-1001303847036]:
+    elif message.chat.id in [-1001489565747,-1001469623910,-1001303847036]:
         try:
             if message.document or message.video:
                 await message.copy(int(Config.FORWARD_TO_CHAT_ID))
@@ -51,7 +51,7 @@ async def main(client, message):
             await client.send_message(chat_id="me", text=f"#FloodWait: Stopping Forwarder for `{e.x}s`!")
             await asyncio.sleep(e.x)
         except Exception as err:
-            await client.send_message(chat_id="me", text=f"#ERROR: `{err}`")'''
+            await client.send_message(chat_id="me", text=f"#ERROR: `{err}`")
 
 
 User.run()
